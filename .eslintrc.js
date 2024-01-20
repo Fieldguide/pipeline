26c699dcb85af51d910c28c74c029229f7bf7de9
Nick Amoscato <nick@amoscato.com>
2024-01-19 22:57:16 -0500

    Add unused-imports/no-unused-imports rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
         "@typescript-eslint/no-unsafe-call": "warn",
         "@typescript-eslint/no-unsafe-member-access": "warn",
         "@typescript-eslint/no-unsafe-return": "warn",
+        "unused-imports/no-unused-imports": "error",
         curly: "error",
       },
     },
